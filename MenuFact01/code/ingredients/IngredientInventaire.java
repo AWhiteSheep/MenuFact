5bc30818c5f285076be7fecfe3bba5ff6947bd58
Naomie <nao15@live.ca>
2021-09-08 09:16:15 -0400

    javadoc IngredientInventaire

--- MenuFact01/code/ingredients/IngredientInventaire.java
+++ MenuFact01/code/ingredients/IngredientInventaire.java
@@ -2,20 +2,39 @@ package ingredients;
 
 import ingredients.exceptions.IngredientException;
 
-//MODEL
+/**
+ * Classe pour les ingredients de linventaire du systeme Menufact
+ * @author Naomie L'archevêque Carrière
+ * @author Yan Ha Routhier-Chevrier
+ * @version 2.0
+ */
 public class IngredientInventaire {
     private Ingredient ingredient;
     private int quantite;
 
+    /**
+     * constructeur
+     * @param ingredient lingredient de linventaire
+     * @param quantite la quantite de lingredient
+     */
     public IngredientInventaire(Ingredient ingredient, int quantite) {
         this.ingredient = ingredient;
         this.quantite = quantite;
     }
 
+    /**
+     *
+     * @return la quantite de lingredient
+     */
     public int getQuantite() {
         return quantite;
     }
 
+    /**
+     * change la quantite de lingredient
+     * @param quantite la quantite a changer de lingredient
+     * @throws IngredientException si la quantite est negative
+     */
     public void setQuantite(int quantite) throws IngredientException{
         if (quantite < 0)
             throw new IngredientException("Il n'est pas possible d'avoir une quantité negative");
@@ -23,5 +42,9 @@ public class IngredientInventaire {
             this.quantite = quantite;
     }
 
+    /**
+     *
+     * @return le nom de lingredient
+     */
     public String getNom(){return ingredient.getNom();}
 }
